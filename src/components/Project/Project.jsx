df4e6d995c1675c1c6ec024d1e7db6dec5bd5fd8
Ranjithbabu0912 <ranjithbabu0912@gmail.com>
2025-12-12 19:09:34 +0530

    projects

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -32,7 +32,7 @@ const Project = () => {
                 <br />
                 <br />
                 <Button>
-                    <a href="/Project" style={{ all: 'unset' }}><p className='project_btn'> More Work</p></a>
+                    <a href="/project" style={{ all: 'unset' }}><p className='project_btn'> More Work</p></a>
                 </Button>
             </div>
         </>
